0880e176793dee8f7910259edc4a08f43f65088c
Harry Liu <harryliu1995@gmail.com>
2019-06-12 05:43:27 -0400

    4 weeks data for column chart

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from google_calendar import calendarAPI
 def index():
     yourcalendar = calendarAPI()
     pie_chart = yourcalendar.getPieChartDataForNext7Days()
-    column_chart = yourcalendar.getColumnChartDataForNextXWeeks(3)
+    column_chart = yourcalendar.getColumnChartDataForNextXWeeks(4)
     return render_template('index.html', pie_chart=pie_chart, column_chart=column_chart)
 
 @app.route("/chart_data")
